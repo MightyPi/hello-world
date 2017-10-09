4da1b7048fd802a25941693fd420c8eb41b50468
MightyPi <Ben@Bens-MacBook-Pro.local>
2017-10-09 19:20:29 -0400

    Scroll fade in javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+// Changing the defaults
+window.sr = ScrollReveal({ reset: true });
 
+window.sr = ScrollReveal();
+sr.reveal('#about', {duration: 300});
+sr.reveal('#interests', {duration: 300});
+sr.reveal('#portfolio', {duration: 300});
+sr.reveal('#contact', {duration: 300});
+sr.reveal('.footer', {duration: 300});
