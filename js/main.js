cf08f86c0b89f821e46e99ac0612916f22e645ee
benfarrall <bfarrall@booknetcanada.ca>
2020-01-03 21:13:09 -0500

    clean up

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Changing the defaults
-window.sr = ScrollReveal({ reset: false });
-
-window.sr = ScrollReveal();
-sr.reveal('#about', {duration: 300});
-sr.reveal('#interests', {duration: 300});
-sr.reveal('#portfolio', {duration: 300});
-sr.reveal('#contact', {duration: 300});
-sr.reveal('.footer', {duration: 300});
